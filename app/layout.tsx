7aba7a1cff064f22484ed114d92015a01b48ff54
Gaurav-2k1 <gauravbhukte1507@gmail.com>
2023-10-04 12:39:06 +0530

    added x

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,11 @@ import { Urbanist } from 'next/font/google'
 
 import ModalProvider from '@/providers/modal-provider'
 import ToastProvider from '@/providers/toast-provider'
-import Navbar from '@/components/navbar'
-import Footer from '@/components/footer'
+
 
 import './globals.css'
+import Navbar from '@/components/navbar'
+import Footer from '@/components/footer'
 
 const font = Urbanist({ subsets: ['latin'] })
 
